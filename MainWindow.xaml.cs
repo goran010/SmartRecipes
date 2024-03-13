8b11bfc49427df0300f385a77d5df3279c4ba436
Goran Marković <91391493+goran010@users.noreply.github.com>
2024-03-13 17:14:05 +0100

    created nav button and second page

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,55 +1,35 @@
-﻿using System;
-using System.Diagnostics;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using Newtonsoft.Json;
-using RecipeApp.services;
-using RecipeApp.View.UserControls;
+using RecipeApp.Pages;
 
 namespace RecipeApp
 {
-    public class RecipeCard
-    {
-        public string? RecipeName { get; set; }
-        public string? ImagePath { get; set; }
-        public string? Country { get; set; }
-    }
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            MaxWidth = SystemParameters.PrimaryScreenWidth;
-            searchBar.SearchResultReceived += UpdateMealsListTextBox;
         }
 
-        private void UpdateMealsListTextBox(List<string[]> recipes)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            // Create a list of RecipeCard objects
-            List<RecipeCard> recipeCards = [];
-
-            // For each string array in the recipes list, create a new RecipeCard and add it to the recipeCards list
-            foreach (string[] recipe in recipes)
+            if (mainFrame.NavigationService != null)
             {
-                // Replace this with your logic to set ImagePath and Description
-                RecipeCard recipeCard = new()
+                try
                 {
-                    //title of recipe
-                    RecipeName = recipe[0],
-                    //recipe image
-                    ImagePath = recipe[1],
-                    //country of recipe
-                    Country = recipe[2]
-                };
-
-                recipeCards.Add(recipeCard);
+                    mainFrame.NavigationService.Navigate(new Pages.ContentRecipesPage());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Navigation error: {ex.Message}");
+                }
             }
-
-            // Set the ItemsSource of the MealsListTextBox to the list of RecipeCard objects
-            MealsListTextBox.ItemsSource = recipeCards;
+            else
+            {
+                MessageBox.Show("NavigationService is null.");
+            }
         }
 
     }
 }
+
