dc2f6916f94708a1e4d214e66901961b5b43334d
Goran Marković <91391493+goran010@users.noreply.github.com>
2024-03-19 00:17:26 +0100

    added favorite meals on home page

--- View/UI/GridPopularMeals.xaml.cs
+++ View/UI/GridPopularMeals.xaml.cs
@@ -1,28 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RecipeApp.View.UI
-{
-    /// <summary>
-    /// Interaction logic for GridPopularMeals.xaml
-    /// </summary>
-    public partial class GridPopularMeals : UserControl
     {
-        public GridPopularMeals()
+    public partial class GridPopularMeals : UserControl
         {
+        public GridPopularMeals ()
+            {
             InitializeComponent();
+            }
+
+        // Method to handle the Click event of the buttons
+        private void MealClicked ( object sender, RoutedEventArgs e )
+            {
+            string mealName = "";
+            // Check if the sender is a Button
+            if (sender is Button clickedButton)
+                {
+                // Extract the meal name from the TextBlock associated with the clicked Button
+                if (clickedButton.Content is Grid buttonContent)
+                    {
+                    foreach (var child in buttonContent.Children)
+                        {
+                        if (child is TextBlock textBlock && textBlock.Name.StartsWith("tBNameOfMeal"))
+                            {
+                            mealName = textBlock.Text;
+                            Console.WriteLine($"Clicked meal: {mealName}");
+                            break;
+                            }
+                        }
+                    }
+                }
+            string country = "";
+            string recipeName = mealName;
+            string imagePath = "";
+            string instructions = "";
+            string category = "Unknown Category";
+
+            // Create an object array with all the string properties
+            object[] recipeDetails = [country, recipeName, imagePath, instructions, category];
+
+            // Navigate to the recipe details page, passing the recipe details
+            ((MainWindow)Application.Current.MainWindow).NavigateToShowRecipePage(recipeDetails);
+            }
         }
     }
-}
