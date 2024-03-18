38613db4c63560ebef9b413df011d35254fabc7a
Petar Car <petar.car135@gmail.com>
2024-03-18 12:48:43 +0100

    Added recipe info site, info is taken from api

--- Pages/ShowRecipe.xaml.cs
+++ Pages/ShowRecipe.xaml.cs
@@ -1,18 +1,48 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
 
 namespace RecipeApp.Pages
+{
+    public partial class ShowRecipe : Page, INotifyPropertyChanged
     {
-    public partial class ShowRecipe : Page
+        private string _country;
+        public string Country
         {
-        // Properties to hold recipe details
-        public string Country { get; set; }
-        public string RecipeName { get; set; }
-        public string ImagePath { get; set; }
-        public string Instructions { get; set; }
-        public string Category { get; set; }
+            get { return _country; }
+            set { _country = value; NotifyPropertyChanged(nameof(Country)); }
+        }
 
-        public ShowRecipe ( string country, string recipeName, string imagePath, string instructions, string category )
-            {
+        private string _recipeName;
+        public string RecipeName
+        {
+            get { return _recipeName; }
+            set { _recipeName = value; NotifyPropertyChanged(nameof(RecipeName)); }
+        }
+
+        private string _imagePath;
+        public string ImagePath
+        {
+            get { return _imagePath; }
+            set { _imagePath = value; NotifyPropertyChanged(nameof(ImagePath)); }
+        }
+
+        private string _instructions;
+        public string Instructions
+        {
+            get { return _instructions; }
+            set { _instructions = value; NotifyPropertyChanged(nameof(Instructions)); }
+        }
+
+        private string _category;
+        public string Category
+        {
+            get { return _category; }
+            set { _category = value; NotifyPropertyChanged(nameof(Category)); }
+        }
+
+        public ShowRecipe(string country, string recipeName, string imagePath, string instructions, string category)
+        {
             InitializeComponent();
 
             // Initialize properties with provided values
@@ -21,6 +51,16 @@ namespace RecipeApp.Pages
             ImagePath = imagePath;
             Instructions = instructions;
             Category = category;
-            }
+
+
+            DataContext = this;
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
+}
